9278795882e51f2450a737ab8d1d431c3a51e2cb
Mik <info@slnt-opp.xyz>
2021-12-05 14:28:46 +0100

    yet another grpc example

---
+++ cmd/tunnelclient/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+
+	pb "github.com/EwRvp7LV7/nocloud-tunnel-mesh/pkg/proto"
+	"google.golang.org/grpc"
+)
+
+
+func main() {
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithInsecure())
+	opts = append(opts, grpc.WithBlock())
+
+	conn, err := grpc.Dial("localhost:10000", opts...)
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewTunnelClient(conn)
+	stream, err := client.InitTunnel(context.Background(), &pb.InitTunnelRequest{Host: "testo"})
+	if err != nil {
+		panic(err)
+	}
+	for {
+		var msg string
+		err := stream.RecvMsg(&msg)
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			panic(err)
+		}
+	}
+}
---
+++ cmd/tunnelserver/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	pb "github.com/EwRvp7LV7/nocloud-tunnel-mesh/pkg/proto"
+)
+
+var (
+	port = flag.Int("port", 10000, "The server port")
+)
+
+type tunnelServer struct {
+	pb.UnimplementedTunnelServer
+
+	conns map[string]pb.Tunnel_InitTunnelServer
+}
+
+func (s *tunnelServer) SendData(ctx context.Context, req *pb.SendDataRequest) (*pb.SendDataResponse, error) {
+	stream, ok := s.conns[req.GetHost()]
+	if !ok {
+		return nil, status.Error(codes.NotFound, "Connection not found")
+	}
+	err := stream.SendMsg(req.GetMessage())
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &pb.SendDataResponse{Result: true}, nil
+}
+
+func (s *tunnelServer) InitTunnel(req *pb.InitTunnelRequest, stream pb.Tunnel_InitTunnelServer) error {
+	fmt.Println("Got streaming connection request")
+	s.conns[req.GetHost()] = stream
+	for {
+		err := stream.RecvMsg(nil)
+		fmt.Printf("Possible Error receiving from Stream: %w\n", err)
+		if err == io.EOF {
+			delete(s.conns, req.GetHost())
+			return nil
+		}
+		if err != nil {
+			delete(s.conns, req.GetHost())
+			return err
+		}
+	}
+}
+
+func newServer() *tunnelServer {
+	s := &tunnelServer{conns: make(map[string]pb.Tunnel_InitTunnelServer)}
+	return s
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterTunnelServer(grpcServer, newServer())
+	fmt.Printf("gRPC-Server Listening on localhost:%d\n", *port)
+	grpcServer.Serve(lis)	
+}
