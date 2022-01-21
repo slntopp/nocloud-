1c05788334a5b02ca647a0c8aebc32090f733199
Mik <info@slnt-opp.xyz>
2022-01-21 09:12:52 +0100

    curl fist

--- hack/client_debug.sh
+++ hack/client_debug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 /tunnelclient
+curl -X POST "http://$DEBUG_HOST/i/am/debug/request" -v
 traceroute "$DEBUG_HOST"
 ping -c 10 "$DEBUG_HOST"
-curl -X POST "http://$DEBUG_HOST/placeholder" -v
